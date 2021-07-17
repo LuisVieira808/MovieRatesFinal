5b5ec538cb43fe622586f16b899261205674dbbd
LuísVieira <aluno22100@ipt.pt>
2021-07-17 20:45:48 +0100

    .

--- MovieRates/Controllers/ReviewsController.cs
+++ MovieRates/Controllers/ReviewsController.cs
@@ -101,7 +101,7 @@ namespace MovieRates.Controllers
         }
 
         // POST: Reviews/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // To protect from   overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
